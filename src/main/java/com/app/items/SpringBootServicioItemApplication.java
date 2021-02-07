bf0dd67624b28a5e057c2afe8b792bf6f39e951f
Andasato <lopezarenasdaniela@gmail.com>
2021-02-07 18:33:24 -0500

    Se realiza creacion de servicios productos e itmes

---
+++ src/main/java/com/app/items/SpringBootServicioItemApplication.java
@@ -0,0 +1,17 @@
+package com.app.items;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@LoadBalancerClient(name = "servicio-productos")
+@EnableFeignClients
+@SpringBootApplication
+public class SpringBootServicioItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootServicioItemApplication.class, args);
+	}
+
+}
