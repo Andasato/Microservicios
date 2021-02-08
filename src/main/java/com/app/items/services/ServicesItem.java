3074e5ff542a0bf5ac94f73ed8457168f092c68d
Andasato <lopezarenasdaniela@gmail.com>
2021-02-07 19:06:28 -0500

    Se realiza pruebas exitosas en postman

--- src/main/java/com/app/items/services/ServicesItem.java
+++ src/main/java/com/app/items/services/ServicesItem.java
@@ -22,7 +22,7 @@ public class ServicesItem  implements IServicesItem{
 	
 	@Override
 	public List<Items> findAll(){
-		List<Producto> productos = Arrays.asList(clienteRest.getForObject("http://localhost:8082/api/productos", Producto[].class));
+		List<Producto> productos = Arrays.asList(clienteRest.getForObject("http://localhost:8088/api/productos", Producto[].class));
 		return productos.stream().map(p -> new Items(p,1)).collect(Collectors.toList());
 	}
 	
@@ -30,7 +30,7 @@ public class ServicesItem  implements IServicesItem{
 	public Items findById(Long id, Integer cantidad) {
 		Map<String, String> pathVariables = new HashMap<String, String>();
 		pathVariables.put("id",id.toString());
-		Producto producto = clienteRest.getForObject("http://localhost:8082/api/productos/{id}", Producto.class, pathVariables);
+		Producto producto = clienteRest.getForObject("http://localhost:8088/api/productos/{id}", Producto.class, pathVariables);
 		return new Items(producto, cantidad);
 	}
 }
